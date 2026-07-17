0109841195ffd57a031402787becf6b666e860eb
arpan0001 <128321330+arpan0001@users.noreply.github.com>
2026-07-18 03:38:06 +0530

    Refactor : Change MoveProcessor core logic

--- Assets/Scripts/Processors/MoveProcessor.cs
+++ Assets/Scripts/Processors/MoveProcessor.cs
@@ -13,185 +13,64 @@ namespace GridPuzzle.Processors
         {
             bool moved = false;
 
-            switch (direction)
+            bool processRows = direction == Direction.Left || direction == Direction.Right;
+            bool reverse = direction == Direction.Right || direction == Direction.Down;
+
+            int lineCount = processRows ? grid.Height : grid.Width;
+            int lineLength = processRows ? grid.Width : grid.Height;
+
+            for (int line = 0; line < lineCount; line++)
             {
-                case Direction.Left:
-                    moved = MoveLeft(grid);
-                    break;
-
-                case Direction.Right:
-                    moved = MoveRight(grid);
-                    break;
-
-                case Direction.Up:
-                    moved = MoveUp(grid);
-                    break;
-
-                case Direction.Down:
-                    moved = MoveDown(grid);
-                    break;
+                moved |= CompressLine(grid, line, lineLength, processRows, reverse);
             }
 
             return moved;
         }
-        private bool CompressRowLeft(GridData grid, int row)
+
+        /// <summary>
+        /// Compresses a single row or column.
+        /// </summary>
+        private bool CompressLine(
+            GridData grid,
+            int line,
+            int length,
+            bool isRow,
+            bool reverse)
         {
             bool moved = false;
 
-            int targetX = 0;
+            int target = reverse ? length - 1 : 0;
 
-            for (int x = 0; x < grid.Width; x++)
+            int start = reverse ? length - 1 : 0;
+            int end = reverse ? -1 : length;
+            int step = reverse ? -1 : 1;
+
+            for (int i = start; i != end; i += step)
             {
-                int value = grid.GetValue(x, row);
+                int x = isRow ? i : line;
+                int y = isRow ? line : i;
+
+                int value = grid.GetValue(x, y);
 
                 if (value == 0)
                     continue;
 
-                if (x != targetX)
+                int targetX = isRow ? target : line;
+                int targetY = isRow ? line : target;
+
+                if (i != target)
                 {
-                    grid.SetValue(targetX, row, value);
-                    grid.SetValue(x, row, 0);
+                    grid.SetValue(targetX, targetY, value);
+                    grid.SetValue(x, y, 0);
 
                     moved = true;
                 }
 
-                targetX++;
+                target += reverse ? -1 : 1;
             }
 
             return moved;
         }
-
-        private bool CompressRowRight(GridData grid, int row)
-        {
-            bool moved = false;
-
-            int targetX = grid.Width - 1;
-
-            for (int x = grid.Width - 1; x >= 0; x--)
-            {
-                int value = grid.GetValue(x, row);
-
-                if (value == 0)
-                    continue;
-
-                if (x != targetX)
-                {
-                    grid.SetValue(targetX, row, value);
-                    grid.SetValue(x, row, 0);
-
-                    moved = true;
-                }
-
-                targetX--;
-            }
-
-            return moved;
-        }
-
-        private bool CompressColumnUp(GridData grid, int column)
-        {
-            bool moved = false;
-
-            int targetY = 0;
-
-            for (int y = 0; y < grid.Height; y++)
-            {
-                int value = grid.GetValue(column, y);
-
-                if (value == 0)
-                    continue;
-
-                if (y != targetY)
-                {
-                    grid.SetValue(column, targetY, value);
-                    grid.SetValue(column, y, 0);
-
-                    moved = true;
-                }
-
-                targetY++;
-            }
-
-            return moved;
-        }
-
-        private bool CompressColumnDown(GridData grid, int column)
-        {
-            bool moved = false;
-
-            int targetY = grid.Height - 1;
-
-            for (int y = grid.Height - 1; y >= 0; y--)
-            {
-                int value = grid.GetValue(column, y);
-
-                if (value == 0)
-                    continue;
-
-                if (y != targetY)
-                {
-                    grid.SetValue(column, targetY, value);
-                    grid.SetValue(column, y, 0);
-
-                    moved = true;
-                }
-
-                targetY--;
-            }
-
-            return moved;
-        }
-    
-
-
-        private bool MoveLeft(GridData grid)
-        {
-            bool moved = false;
-
-            for (int y = 0; y < grid.Height; y++)
-            {
-                moved |= CompressRowLeft(grid, y);
-            }
-
-            return moved;
-        }
-
-        private bool MoveRight(GridData grid)
-        {
-            bool moved = false;
-
-            for (int y = 0; y < grid.Height; y++)
-            {
-                moved |= CompressRowRight(grid, y);
-            }
-
-            return moved;
-        }
-
-        private bool MoveUp(GridData grid)
-        {
-            bool moved = false;
-
-            for (int x = 0; x < grid.Width; x++)
-            {
-                moved |= CompressColumnUp(grid, x);
-            }
-
-            return moved;
-        }
-
-        private bool MoveDown(GridData grid)
-        {
-            bool moved = false;
-
-            for (int x = 0; x < grid.Width; x++)
-            {
-                moved |= CompressColumnDown(grid, x);
-            }
-
-            return moved;
-        }
-
     }
-
 }
+
