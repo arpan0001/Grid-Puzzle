1d5423928018bb225382808c3112190e075d5335
arpan0001 <128321330+arpan0001@users.noreply.github.com>
2026-07-18 03:47:56 +0530

    Fix : add one line in script

--- Assets/Scripts/Data/MoveResult.cs
+++ Assets/Scripts/Data/MoveResult.cs
@@ -3,9 +3,8 @@ namespace GridPuzzle.Data
     public struct MoveResult
     {
         public bool BoardChanged;
-
+        public bool TileMerged;
         public int ScoreGained;
-
         public bool HasWon;
     }
 }
